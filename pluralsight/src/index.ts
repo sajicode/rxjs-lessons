fd086ae405eca5dddbd9da4d6b3dddab28276144
sajicode <sajioloye@gmail.com>
2020-05-03 05:44:42 +0100

    create observables to handle events

--- pluralsight/src/index.ts
+++ pluralsight/src/index.ts
@@ -4,36 +4,48 @@ import { allBooks, allReaders } from './data';
 // * an observable is not executed until an object subscribes to it
 
 // * add $ sign to all variables that would store an observale
-const allBooksObservable$ = Observable.create((subscriber: any) => {
+// const allBooksObservable$ = Observable.create((subscriber: any) => {
 
-  if (allBooks.length !== 6) {
-    subscriber.error('Incorrect list');
-  }
+//   if (allBooks.length !== 6) {
+//     subscriber.error('Incorrect list');
+//   }
 
-  for (const book of allBooks) {
-    subscriber.next(book);
-  }
+//   for (const book of allBooks) {
+//     subscriber.next(book);
+//   }
 
-  setTimeout(() => {
-    subscriber.complete();
-  }, 2000);
+//   setTimeout(() => {
+//     subscriber.complete();
+//   }, 2000);
 
-  return () => console.log('Executing cleanup');
-});
+//   return () => console.log('Executing cleanup');
+// });
 
 // allBooksObservable$.subscribe((book: any) => console.log(book.title));
 
 // * of - can pass in a bunch of values that we want the observable to produce
-const source1$ = of('hello', 23, true, allReaders[0].name);
+// const source1$ = of('hello', 23, true, allReaders[0].name);
 
 // source1$.subscribe((value) => console.log(value));
 
 // * from - pass in a single object that already encapsulates a group of values
 
-const source2$ = from(allBooks);
+// const source2$ = from(allBooks);
 
 // source2$.subscribe((book) => console.log(book.title));
 
 // * concat - used to combine observables
-concat(source1$, source2$).subscribe((values) => console.log(values));
+// concat(source1$, source2$).subscribe((values) => console.log(values));
 
+// * fromEvent - observable to handle events
+const button: any = document.getElementById('readersButton');
+
+fromEvent(button, 'click').subscribe((event) => {
+  console.log(event);
+
+  const readersDiv: any = document.getElementById('readers');
+
+  for (const reader of allReaders) {
+    readersDiv.innerHTML += reader.name + '<br>';
+  }
+});
