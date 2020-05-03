fbd452242bbe0b404da1093ff48f082237b6523d
sajicode <sajioloye@gmail.com>
2020-05-03 06:52:32 +0100

    cancelling observables with a subscription

--- pluralsight/src/index.ts
+++ pluralsight/src/index.ts
@@ -1,4 +1,4 @@
-import { Observable, of, from, fromEvent, concat } from 'rxjs';
+import { Observable, of, from, fromEvent, concat, interval } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 import { allBooks, allReaders } from './data';
 
@@ -97,20 +97,57 @@ import { allBooks, allReaders } from './data';
 // * there can be multiple observers to an observable and
 // * each call to subscribe triggers an independent execution for a particular observer
 
-const currentTime$ = new Observable((subscriber) => {
-  const timeString = new Date().toLocaleTimeString();
-  subscriber.next(timeString);
-  subscriber.complete();
+// const currentTime$ = new Observable((subscriber) => {
+//   const timeString = new Date().toLocaleTimeString();
+//   subscriber.next(timeString);
+//   subscriber.complete();
+// });
+
+// currentTime$.subscribe((currentTime) => console.log(`Observer 1: ${currentTime}`));
+
+// setTimeout(() => {
+//   currentTime$.subscribe((currentTime) => console.log(`Observer 2: ${currentTime}`));
+// }, 1000);
+
+// setTimeout(() => {
+//   currentTime$.subscribe((currentTime) => console.log(`Observer 3: ${currentTime}`));
+// }, 2000);
+
+// * cancelling observable execution
+const timesDiv: any = document.getElementById('times');
+const button: any = document.getElementById('timerButton');
+
+// * interval returns an observable that produces a stream of integers at every specified time
+// const timer$ = interval(1000);
+const timer$ = new Observable((subscriber) => {
+  let i = 0;
+  const intervalID = setInterval(() => {
+    subscriber.next(i++);
+  }, 1000);
+
+  return () => {
+    console.log('Clean Up code running');
+    clearInterval(intervalID);
+  }
 });
 
-currentTime$.subscribe((currentTime) => console.log(`Observer 1: ${currentTime}`));
+const timerSubscription = timer$.subscribe(
+  (value) => timesDiv.innerHTML += `${new Date().toLocaleTimeString()} (${value}) <br>`,
+  null,
+  () => console.log('All done here!'),
+);
 
-setTimeout(() => {
-  currentTime$.subscribe((currentTime) => console.log(`Observer 2: ${currentTime}`));
-}, 1000);
+// * chain subscriptions so that an unscubscribe click on one subscriber cancels both
+const timerConsoleSubscription = timer$.subscribe(
+  (value) => console.log(`${new Date().toLocaleTimeString()} (${value})`),
+);
 
-setTimeout(() => {
-  currentTime$.subscribe((currentTime) => console.log(`Observer 3: ${currentTime}`));
-}, 2000);
+timerSubscription.add(timerConsoleSubscription);
 
+// * remove an added subscription
+// timerSubscription.remove(timerConsoleSubscription);
+
+fromEvent(button, 'click').subscribe((event) => timerSubscription.unsubscribe());
+
+// * when an observable is cancelled by unsubscribe, there would be no complete function to be called
 //#endregion
